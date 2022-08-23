e606cfce76f6858d2760033a1a827e21a1daf02a
Chris Tomy <me@cyberchris.xyz>
2022-08-23 17:53:39 +0100

    git init basic bot

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using dotenv.net;
+using Telegram.Bot;
+
+DotEnv.Load();
+
+var botClient = new TelegramBotClient(Environment.GetEnvironmentVariable("TELEGRAM_ACCESS_TOKEN"));
+
+var me = await botClient.GetMeAsync();
+Console.WriteLine($"Hello, World! I am user {me.Id} and my name is {me.FirstName}.");
