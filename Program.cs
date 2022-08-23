7c2cd0cd0e6c9cd2bf69f69fd36fe29ea6aa068d
Chris Tomy <me@cyberchris.xyz>
2022-08-23 20:37:12 +0100

    follow next example

--- Program.cs
+++ Program.cs
@@ -1,10 +1,56 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using dotenv.net;
 using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 
 DotEnv.Load();
 
 var botClient = new TelegramBotClient(Environment.GetEnvironmentVariable("TELEGRAM_ACCESS_TOKEN"));
 
+using var cts = new CancellationTokenSource();
+
+var receiverOptions = new ReceiverOptions
+{
+    AllowedUpdates = Array.Empty<UpdateType>()
+};
+botClient.StartReceiving(
+    updateHandler: HandleUpdateAsync,
+    pollingErrorHandler: HandlePollingErrorAsync,
+    receiverOptions: receiverOptions,
+    cancellationToken: cts.Token
+);
+
 var me = await botClient.GetMeAsync();
-Console.WriteLine($"Hello, World! I am user {me.Id} and my name is {me.FirstName}.");
+
+Console.WriteLine($"Starting {me.Username}");
+Console.ReadLine();
+
+cts.Cancel();
+
+async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+{
+    if (update.Message is not { } message)
+        return;
+    if (message.Text is not { } messageText)
+        return;
+
+    var chatId = message.Chat.Id;
+
+    Console.WriteLine($"Received a message '{messageText} in chat '{chatId}'");
+    await botClient.SendTextMessageAsync(chatId: chatId, text: $"You said: {messageText}", cancellationToken: cancellationToken);
+}
+
+Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+{
+    var ErrorMessage = exception switch
+    {
+        ApiRequestException apiRequestException => $"",
+        _ => exception.ToString()
+    };
+
+    Console.WriteLine(ErrorMessage);
+    return Task.CompletedTask;
+}
