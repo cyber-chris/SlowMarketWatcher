43ceaf345531bf6c4ba5136bc4def91078464951
Chris Tomy <me@cyberchris.xyz>
2022-08-24 11:00:32 +0100

    restructure to use OOP

--- Program.cs
+++ Program.cs
@@ -6,51 +6,60 @@ using Telegram.Bot.Polling;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 
-DotEnv.Load();
-
-var botClient = new TelegramBotClient(Environment.GetEnvironmentVariable("TELEGRAM_ACCESS_TOKEN"));
-
-using var cts = new CancellationTokenSource();
-
-var receiverOptions = new ReceiverOptions
+namespace SlowMarketWatcher
 {
-    AllowedUpdates = Array.Empty<UpdateType>()
-};
-botClient.StartReceiving(
-    updateHandler: HandleUpdateAsync,
-    pollingErrorHandler: HandlePollingErrorAsync,
-    receiverOptions: receiverOptions,
-    cancellationToken: cts.Token
-);
-
-var me = await botClient.GetMeAsync();
-
-Console.WriteLine($"Starting {me.Username}");
-Console.ReadLine();
-
-cts.Cancel();
-
-async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
-{
-    if (update.Message is not { } message)
-        return;
-    if (message.Text is not { } messageText)
-        return;
-
-    var chatId = message.Chat.Id;
-
-    Console.WriteLine($"Received a message '{messageText} in chat '{chatId}'");
-    await botClient.SendTextMessageAsync(chatId: chatId, text: $"You said: {messageText}", cancellationToken: cancellationToken);
-}
-
-Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
-{
-    var ErrorMessage = exception switch
+    class Program
     {
-        ApiRequestException apiRequestException => $"",
-        _ => exception.ToString()
-    };
+        static async Task Main(string[] args)
+        {
+            DotEnv.Load();
 
-    Console.WriteLine(ErrorMessage);
-    return Task.CompletedTask;
+            var botClient = new TelegramBotClient(Environment.GetEnvironmentVariable("TELEGRAM_ACCESS_TOKEN"));
+
+            using var cts = new CancellationTokenSource();
+
+            var receiverOptions = new ReceiverOptions
+            {
+                AllowedUpdates = Array.Empty<UpdateType>()
+            };
+            botClient.StartReceiving( // note that this does not block
+                updateHandler: HandleUpdateAsync,
+                pollingErrorHandler: HandlePollingErrorAsync,
+                receiverOptions: receiverOptions,
+                cancellationToken: cts.Token
+            );
+
+            var me = await botClient.GetMeAsync();
+
+            Console.WriteLine($"Starting {me.Username}");
+            Console.ReadLine();
+
+            cts.Cancel();
+        }
+
+        static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            if (update.Message is not { } message)
+                return;
+            if (message.Text is not { } messageText)
+                return;
+
+            var chatId = message.Chat.Id;
+
+            Console.WriteLine($"Received a message '{messageText} in chat '{chatId}'");
+            await botClient.SendTextMessageAsync(chatId: chatId, text: $"You said: {messageText}", cancellationToken: cancellationToken);
+        }
+
+        static Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            var ErrorMessage = exception switch
+            {
+                ApiRequestException apiRequestException => $"",
+                _ => exception.ToString()
+            };
+
+            Console.WriteLine(ErrorMessage);
+            return Task.CompletedTask;
+        }
+    }
 }
